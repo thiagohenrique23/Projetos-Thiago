f8fc80d11de3c5d5a4b3c8e047c65e24316c8420
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-04-05 16:27:58 -0300

    erro

--- projeto-lol/projeto/src/index.js
+++ projeto-lol/projeto/src/index.js
@@ -1,31 +1,10 @@
-import express from "express"
-import { json } from "express"
-import axios from "axios"
-import cors from "cors"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-require("dotenv").config()
-
-const app = express()
-
-app.use(json())
-app.use(cors())
-app.listen(3333)
-
-app.get("/summoner/:summonerName", async(req, res) => {
-  const { summonerName } = req.params
-  
-  const summonerIdResponse = await axios
-  .get(`${process.env.LOL_URL}/lol/summoner/v4/summoners/by-name/${summonerName}`,
-  { headers: { 'X-Riot-Token': process.env.LOL_KEY } } )
-  .catch(e => {
-    return res.status(e.response.status).json(e.response.data)
-  })
-
-  const { id, profileIconId, summonerLevel } = summonerIdResponse.data
-
-  const responseRanked = await axios.get(`${process.env.LOL_URL}/lol/league/v4/entries/by-summoner/${id}`,
-  { headers: { 'X-Riot-Token': process.env.LOL_KEY } } )
-  .catch(e => {
-    return res.status(e.response.status).json(e.response.data)
-  })
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
