ce80b29be4a7e627de575f1e748a285c8dd143a6
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-04-20 08:56:59 -0300

    Atualização 2

--- thiagro/src/App.js
+++ thiagro/src/App.js
@@ -1,14 +1,24 @@
-import { ThemeProvider } from "@mui/material";
-import React from "react"
-import theme from "./constants/theme";
-import Router from "./routes/Router";
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router/>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- thiagro/src/components/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import Toolbar from "@material-ui/core/Toolbar";
-
-export const StyledToolBar = styled(Toolbar)`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-` 
